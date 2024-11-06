15f35e4fa8a3934c56249887810890a5c243509f
James Clarke <james.clarke2@tudublin.ie>
2024-11-06 11:47:24 +0000

    Menu with buttons

--- addons/immersive-home-ui/lib/globals/main.gd
+++ addons/immersive-home-ui/lib/globals/main.gd
@@ -1,9 +1,11 @@
 extends Node
 
+const Main = preload ("res://content/main.gd")
 const Menu = preload ("res://addons/immersive-home-ui/content/ui/menu/menu.gd")
 const ControllerLeft = preload ("res://addons/immersive-home-ui/content/system/controller_left/controller_left.gd")
 const ControllerRight = preload ("res://addons/immersive-home-ui/content/system/controller_right/controller_right.gd")
 
+@onready var main: Main = get_node_or_null("/root/Main")
 @onready var menu: Menu = get_node_or_null("/root/Main/Menu")
 @onready var camera: XRCamera3D = get_node_or_null("/root/Main/XROrigin3D/XRCamera3D")
 @onready var controller_left: ControllerLeft = get_node_or_null("/root/Main/XROrigin3D/XRControllerLeft")
