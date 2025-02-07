2ec97fa18bff6c806bb5eb39d6572e574ee00516
timmno12 <benjamintimm21@gmail.com>
2025-02-07 11:27:29 +0100

    removed AH

--- addons/immersive-home-ui/content/ui/components/button/button.gd
+++ addons/immersive-home-ui/content/ui/components/button/button.gd
@@ -15,9 +15,9 @@ const ECHO_WAIT_REPEAT = 0.1
 @onready var panel: Panel3D = $Body/Panel3D
 @onready var collision: CollisionShape3D = $Body/CollisionShape3D
 @onready var label_node: Label3D = $Body/Label
-@onready var finger_area: Area3D = $FingerArea
-@onready var finger_area_collision: CollisionShape3D = $FingerArea/CollisionShape3D
 @onready var click_sound = $ClickSound
+
+@onready var fingerArea: Area3D = $FingerArea
 
 @export var focusable: bool = true:
 	set(value):
@@ -45,15 +45,20 @@ const ECHO_WAIT_REPEAT = 0.1
 		if !is_inside_tree(): return
 		
 		if icon:
-			#label_node.font = IconFont
-			label_node.font_size = size.x / 0.05 * 36
-			label_node.width = 1000
-			label_node.autowrap_mode = TextServer.AUTOWRAP_OFF
+			$Body/Label.visible = false
+			$Body/Icon.visible = true
 		else:
-			label_node.font = null
-			label_node.font_size = font_size
-			label_node.width = size.x / label_node.pixel_size
-			label_node.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+			$Body/Label.visible = true
+			$Body/Icon.visible = false
+		
+@export var icon_image : Texture2D = null:
+	set(value):
+		icon_image = value
+		if icon_image:
+			$Body/Icon.texture = icon_image
+		else:
+			$Body/Icon.texture = null
+			
 
 @export var toggleable: bool = false
 @export var disabled: bool = false
@@ -85,6 +90,18 @@ func _ready():
 
 	_update()
 
+	if fingerArea.get_script() == null:
+		var script = GDScript.new()
+		script.source_code = """
+extends StaticBody3D
+func pointer_event(event):
+	get_parent()._handle_pointer_event(event)
+"""
+		script.reload()
+		fingerArea.set_script(script)
+	elif not fingerArea.has_method("pointer_event"):
+		push_error("Button3D: The StaticBody3D node must have a pointer_event method to work properly.")
+
 	Update.props(self, ["active", "external_value", "icon", "label", "font_size", "disabled"])
 
 	if echo:
@@ -100,111 +117,65 @@ func _ready():
 		)
 
 		add_child(echo_timer)
+func _on_echo_timeout():
+	echo_timer.wait_time = ECHO_WAIT_REPEAT
+	on_button_down.emit()
 
 func update_animation(value: float):
 	var tween = create_tween()
 	tween.set_parallel(true)
-
 	tween.tween_property(body, "scale:z", lerpf(1.0, 0.5, value), 0.2)
-	tween.tween_property(body, "position:z", lerpf(size.z / 2, size.z / 4, value), 0.2)
-		
-func _on_press_down(event):
-	if disabled:
-		event.bubbling = false
-		return
+	tween.tween_property(body, "position:z", lerpf(size.z/2, size.z/4, value), 0.2)
 
-	click_sound.play()
-
-	if toggleable:
-		return
-
-	if echo:
-		echo_timer.start()
-
-	active = true
-	on_button_down.emit()
+func _handle_pointer_event(event: XRToolsPointerEvent):
+	if event.target != body: return  # Ensure events are for our body
 	
-func _on_press_up(event):
-	if disabled:
-		event.bubbling = false
-		return
+	match event.event_type:
+		XRToolsPointerEvent.Type.ENTERED:
+			_on_pointer_entered(event)
+		XRToolsPointerEvent.Type.EXITED:
+			_on_pointer_exited(event)
+		XRToolsPointerEvent.Type.PRESSED:
+			_on_pointer_pressed(event)
+		XRToolsPointerEvent.Type.RELEASED:
+			_on_pointer_released(event)
 
-	if toggleable:
-		active = !active
-
-		if active:
-			on_button_down.emit()
-		else:
-			on_button_up.emit()
-	else:
-		if echo:
-			echo_timer.stop()
-			echo_timer.wait_time = ECHO_WAIT_INITIAL
-
-		active = false
-		on_button_up.emit()
-
-func _on_ray_enter(_event: EventPointer):
-	if disabled:
-		return
-
+func _on_pointer_entered(event: XRToolsPointerEvent):
+	if disabled: return
 	panel.hovering = true
 
-func _on_ray_leave(_event: EventPointer):
+func _on_pointer_exited(event: XRToolsPointerEvent):
 	panel.hovering = false
 
-func _on_touch_enter(event: EventTouch):
-	if event.target != finger_area:
-		return
-
-	if disabled:
-		event.bubbling = false
-		return
-
+func _on_pointer_pressed(event: XRToolsPointerEvent):
+	if disabled: return
+	
 	click_sound.play()
-
-	if toggleable:
-		active = !active
-		if active:
-			on_button_down.emit()
-		else:
-			on_button_up.emit()
-
-		return
-
+	
+	if toggleable: 
+		return  # Toggle handled on release
+	
+	if echo:
+		echo_timer.start()
+	
 	active = true
 	on_button_down.emit()
 
-	_touch_change(event)
-
-func _on_touch_move(event: EventTouch):
-	if disabled:
-		event.bubbling = false
-		return
-
+func _on_pointer_released(event: XRToolsPointerEvent):
+	if disabled: return
+	
 	if toggleable:
-		return
-
-	_touch_change(event)
-
-func _on_touch_leave(event: EventTouch):
-	if disabled:
-		event.bubbling = false
-		return
-
-	if toggleable:
-		return
-
-	active = false
-	on_button_up.emit()
-
-func _touch_change(event: EventTouch):
-	var click_pos = to_local(event.fingers[0].area.global_position)
-
-	var percent = (click_pos.z - size.z / 2) / (size.z / 2)
-	percent = clamp(percent, 0.0, 1.0)
-		
-	update_animation(percent)
+		active = !active
+		if active: 
+			on_button_down.emit()
+		else: 
+			on_button_up.emit()
+	else:
+		if echo: 
+			echo_timer.stop()
+			echo_timer.wait_time = ECHO_WAIT_INITIAL
+		active = false
+		on_button_up.emit()
 
 func _update():
 	body.position = Vector3(0, 0, size.z / 2)
@@ -214,6 +185,12 @@ func _update():
 	collision.shape.size = Vector3(size.x, size.y, size.z)
 	label_node.width = size.x / label_node.pixel_size
 	label_node.position = Vector3(0, 0, size.z / 2 + 0.001)
-
-	finger_area.position = Vector3(0, 0, size.z * 0.75)
-	finger_area_collision.shape.size = Vector3(size.x, size.y, size.z / 2)
+	
+	## update icon scale based on button size
+	if icon:
+		var base_icon_scale = 0.3
+		var scale_factor = 1.0
+		# If the button's height is less than 0.03, adjust the icon's scale proportionally.
+		if size.x < 0.03 or size.y < 0.03:
+			scale_factor = min(size.x, size.y) / 0.03
+		$Body/Icon.scale = Vector3(base_icon_scale * scale_factor, base_icon_scale * scale_factor, base_icon_scale * scale_factor)
